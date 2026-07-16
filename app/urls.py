fbec40569fe5ce011101cfa49f8a9020705853d9
GUSTAVO OLIVEIRA PIRES <gustavo.pires@educacao.senac.rio>
2026-07-16 19:30:26 -0300

    gabarito exercicio 16/07

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='app'),
+    path('perfil/', views.perfil_view, name='perfil'),
+    path('status/', views.status_view, name='status'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -3,10 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     context = {
-        'instrutor': 'Diego',
-        'curso': 'Front-end',
-        'carga_horaria': '20 horas',
-        'nivel': 'Iniciante'
+        'nome_empresa': 'TechStore 🐸'
     }
-    # Caminho com namespace
     return render(request,'home.html', context)
+
+def perfil_view(request):
+    context = {
+        'nome_funcionario': 'Gustavo',
+        'cargo': 'Professor',
+        'setor': 'T.I.',
+    }
+    return render(request,'perfil.html', context)
+
+def status_view(request):
+    context = {
+        'id_servidor': '145761',
+        'status_sistema': '200 OK',
+    }
+    return render(request,'status.html', context)
