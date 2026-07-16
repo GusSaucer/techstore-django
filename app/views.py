3bfa2041dbfbaa85614f37a0346eb085583f66be
GUSTAVO OLIVEIRA PIRES <gustavo.pires@educacao.senac.rio>
2026-07-16 20:40:43 -0300

    Aula 09

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     context = {
-        'nome_empresa': 'TechStore 🐸'
+        'nome_empresa': 'TechStore 🐸',
+        'produtos': ['Smartphone', 'PC Gamer', 'Monitor', 'Smartwatch', 'Cadeira Gamer']
     }
     return render(request,'home.html', context)
 
@@ -19,5 +20,6 @@ def status_view(request):
     context = {
         'id_servidor': '145761',
         'status_sistema': '200 OK',
+        'admin':False,
     }
     return render(request,'status.html', context)
