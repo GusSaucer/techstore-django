4cb4e9a975dd07af1142f83afa8c10eb7d1ad2f4
GUSTAVO OLIVEIRA PIRES <gustavo.pires@educacao.senac.rio>
2026-08-11 20:37:37 -0300

    20:40

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='app'),
+    path('', views.home_view, name='home'),
     path('perfil/', views.perfil_view, name='perfil'),
     path('produtos/', views.produtos_view, name='produtos'),
 ]
