9e0736ee63782da90f8dd2108d550415aaa1d026
GUSTAVO OLIVEIRA PIRES <gustavo.pires@educacao.senac.rio>
2026-08-11 19:33:46 -0300

    19:33

--- app/models.py
+++ app/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-# Create your models here.
-
 class Produto(models.Model):
-    nome = models.CharField(max_length=200) #CharField = tipo string
-    preco = models.FloatField() # tipo float
-    estoque = models.IntegerField() # tipo inteiro
-    desconto = models.BooleanField() # tipo booleano
-    def __str__(self): # Método Getter (POO)
-        return self.nome
+    # 'blank=True, null=True' evita erros se o produto não tiver imagem cadastrada
+    imagem = models.CharField(max_length=500, blank=True, null=True) 
+    
+    nome = models.CharField(max_length=200)
+    preco = models.FloatField()
+    estoque = models.IntegerField()
+    desconto = models.BooleanField(default=False)
+    
+    # 'default=False' garante que produtos existentes não iniciem marcados como apagados
+    apagado = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.nome
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from.models import Produto
 from .forms import ProdutoForm
 
