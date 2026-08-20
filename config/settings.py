e00049645c717eac09ca2dc283de7b63ad0520ef
Gustavo Pires <108151584+GusSaucer@users.noreply.github.com>
2026-08-20 20:40:33 -0300

    Modify settings for production deployment
    Updated Django settings for production environment.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h173^i*#d$0x-a!-1@zfd&6gwwp%lap*kwv9)*l7(s-f=cf&%t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
